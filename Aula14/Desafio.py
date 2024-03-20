cb09f778c933c40ce10893b5c7860c55b462330c
GabrielG-tech <gabriel@mpsouza.com.br>
2024-03-20 10:32:31 -0300

    .

---
+++ Aula14/Desafio.py
@@ -0,0 +1,2 @@
+# 1) Elabore uma tabela do Python com os campos ID e valor. Em seguida, insira 1.000.000 registros randômicos 1 a 100 no mesmo. Finalmente, meça a performance da tabela usando o BETWEEN e OPERADOR RELACIONAL para buscar valores entre 30 e 70 e veja qual o método de busca é mais eficiente.
+
